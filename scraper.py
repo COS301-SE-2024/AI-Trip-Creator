64ce174556198fe57b4b35160341b3df9370294e
Rampa47 <134211773+Rampa47@users.noreply.github.com>
2024-07-23 19:44:50 +0200

    Updated scraper.py file

--- scraper.py
+++ scraper.py
@@ -3,18 +3,25 @@ import requests
 import time
 import re
 
-location = input("Which location do you want to look for accommodation ? \n")
+def extract_link(location):
+    url = f'https://www.booking.com/{location}'
 
-url = f'https://www.booking.com/{location}'
+    Page_contents = requests.get(url).text
 
-Page_contents = requests.get(url).text
+    soup = BeautifulSoup(Page_contents,"html.parser")
 
-soup = BeautifulSoup(Page_contents,"html.parser")
+    pages = soup.find_all('a', href=re.compile(r"https://www.booking.com/hotel/.*\.html"))
 
-pages = soup.find_all('a', href=re.compile(r"https://www.booking.com/hotel/.*\.html"))
+    links = [link.get("href") for link in pages]
 
-links = [link.get("href") for link in pages]
+    print("Accommodation links found:")
+    for link in links:
+        print(link)
 
-print("Accommodation links found:")
-for link in links:
-    print(link)
+def main():
+    location = input("Which location do you want to look for accommodation ? \n")
+
+    extract_link(location)
+
+if __name__ == "__main__":
+    main()
