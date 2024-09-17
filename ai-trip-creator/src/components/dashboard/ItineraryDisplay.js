ff8126f542ee3297d96614b93e4151150b0db2e8
Lerato Gololo <leratoegololo2@gmail.com>
2024-09-18 01:30:56 +0200

    Update ItineraryDisplay.js
    flights option in itinerary

--- ai-trip-creator/src/components/dashboard/ItineraryDisplay.js
+++ ai-trip-creator/src/components/dashboard/ItineraryDisplay.js
@@ -13,7 +13,14 @@ import { getGlobalAIText } from "./globalData";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm"; // For GitHub Flavored Markdown (e.g., tables, strikethroughs)
 import Slider from "react-slick"; // Import Slider from react-slick
-import { FaFilter, FaSearch, FaStar, FaHeart } from "react-icons/fa";
+import {
+  FaFilter,
+  FaSearch,
+  FaStar,
+  FaHeart,
+  FaPlaneDeparture,
+  FaPlaneArrival,
+} from "react-icons/fa";
 import "./dashboard.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -25,10 +32,106 @@ import {
   getDocs,
 } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+const client_id = "rwfsFIbmTtMXDAAjzXKCBcR6lZZirbin";
+const client_secret = "RGeFEPqnTMNFKNjd";
 
+const convertPriceToZar = (price, fromCurrency) => {
+  if (fromCurrency === "EUR") {
+    const exchangeRate = 19.21; // Hardcoded exchange rate from EUR to ZAR
+    return price * exchangeRate;
+  } else {
+    console.warn(`Conversion from ${fromCurrency} to ZAR is not supported.`);
+    return null;
+  }
+};
+
+const getFlightOffers = async (
+  originLocation,
+  destinationLocation,
+  departureDate,
+  adults,
+  maxOffers,
+) => {
+  const tokenUrl = "https://test.api.amadeus.com/v1/security/oauth2/token";
+
+  try {
+    const tokenResponse = await fetch(tokenUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        client_id: client_id,
+        client_secret: client_secret,
+        grant_type: "client_credentials",
+      }),
+    });
+
+    if (!tokenResponse.ok) {
+      const errorText = await tokenResponse.text();
+      console.error(
+        `Token request failed: ${tokenResponse.statusText}`,
+        errorText,
+      );
+      throw new Error(`Token request failed: ${tokenResponse.statusText}`);
+    }
+
+    const tokenData = await tokenResponse.json();
+    const accessToken = tokenData.access_token;
+
+    const searchUrl = new URL(
+      "https://test.api.amadeus.com/v2/shopping/flight-offers",
+    );
+    const params = {
+      originLocationCode: originLocation,
+      destinationLocationCode: destinationLocation,
+      departureDate: departureDate,
+      adults: adults.toString(),
+      max: maxOffers.toString(),
+    };
+
+    Object.keys(params).forEach((key) =>
+      searchUrl.searchParams.append(key, params[key]),
+    );
+    console.log(`Request URL: ${searchUrl}`);
+
+    const searchResponse = await fetch(searchUrl.toString(), {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!searchResponse.ok) {
+      const errorText = await searchResponse.text();
+      console.error(
+        `Search request failed: ${searchResponse.statusText}`,
+        errorText,
+      );
+      throw new Error(`Search request failed: ${searchResponse.statusText}`);
+    }
+
+    const flightData = await searchResponse.json();
+    console.log("Flight data:", flightData); // Debugging line
+
+    for (const flight of flightData.data) {
+      const originalPrice = parseFloat(flight.price.total);
+      const fromCurrency = flight.price.currency;
+      const priceInZar = await convertPriceToZar(originalPrice, fromCurrency);
+      flight.priceInZar = priceInZar ? priceInZar.toFixed(2) : "N/A";
+      console.log(`Price in ZAR for flight: ${flight.priceInZar}`); // Debugging line
+    }
+
+    return flightData.data;
+  } catch (error) {
+    console.error("Error fetching flight offers:", error);
+    return null;
+  }
+};
 function ItineraryDisplay({ itinerary }) {
   const [globalAIText, setGlobalAIText] = useState("");
   const [accommodations, setAccommodations] = useState([]);
+  const [flights, setFlights] = useState([]);
 
   useEffect(() => {
     // Directly set the AI-generated itinerary from the passed itinerary prop
@@ -37,6 +140,59 @@ function ItineraryDisplay({ itinerary }) {
     }
     const destinationParam = itinerary.destination;
     handleSearch(destinationParam.toLowerCase().replace(/\s+/g, ""));
+    const fetchData = async () => {
+      try {
+        const date = new Date().toISOString().split("T")[0];
+        const loc = itinerary.currentLocation.toLowerCase();
+        const des = itinerary.destination.toLowerCase();
+        let originLocation;
+        if (loc === "johannesburg") {
+          originLocation = "JNB";
+        } else if (loc === "pretoria" && des !== "johannesburg") {
+          originLocation = "JNB";
+        } else if (loc === "durban") {
+          originLocation = "DUR";
+        } else if (loc === "cape town") {
+          originLocation = "CPT";
+        } else if (loc === "gqeberha") {
+          originLocation = "PLZ";
+        } else {
+          console.error("Unknown location: " + loc);
+        }
+
+        let destinationLocation;
+        if (des === "johannesburg") {
+          destinationLocation = "JNB";
+        } else if (des === "pretoria" && loc !== "johannesburg") {
+          destinationLocation = "JNB";
+        } else if (des === "durban") {
+          destinationLocation = "DUR";
+        } else if (des === "cape town") {
+          destinationLocation = "CPT";
+        } else if (des === "gqeberha") {
+          destinationLocation = "PLZ";
+        } else {
+          console.error("Unknown location: " + loc);
+        }
+
+        const flightOffers = await getFlightOffers(
+          //JNB,DUR,CPT,PLZ,
+          originLocation,
+          destinationLocation,
+          date,
+          1,
+          10,
+        );
+        if (flightOffers) {
+          setFlights(flightOffers);
+        } else {
+          console.log("No flight offers available.");
+        }
+      } catch (error) {
+        console.error("Error fetching flight data:", error);
+      }
+    };
+    fetchData();
   }, [itinerary]);
 
   const getDetails = () => {
@@ -190,7 +346,7 @@ function ItineraryDisplay({ itinerary }) {
                         <FaStar color="#FFD700" size={15} /> {"   "}
                         {accommodation.rating}
                         <br />
-                        {`R${accommodation.price}/night`} <br />
+                        {`from R${accommodation.price}/night`} <br />
                         click to book accomodation
                       </Typography>
 
@@ -215,9 +371,54 @@ function ItineraryDisplay({ itinerary }) {
       <br />
       <Card mt={2}>
         <CardContent>
-          <Typography sx={{ mt: 1 }} variant="outlined" color="secondary">
-            Flight options
+          <Typography variant="h5" gutterBottom sx={{ mt: 4 }}>
+            Flight Options
           </Typography>
+
+          {flights.length === 0 ? (
+            <Typography color="textSecondary">
+              No flight offers available.
+            </Typography>
+          ) : (
+            <Grid container spacing={2} mt={2}>
+              {flights.map((flight, index) => {
+                const { segments } = flight.itineraries[0];
+                const firstSegment = segments[0];
+                const lastSegment = segments[segments.length - 1];
+
+                const departureTime = firstSegment.departure.at.split("T")[1];
+                const arrivalTime = lastSegment.arrival.at.split("T")[1];
+                const carrierCode = firstSegment.carrierCode;
+                const totalPrice = flight.price.total;
+                const totalPriceInZar = flight.priceInZar;
+
+                return (
+                  <Grid item xs={12} sm={6} md={4} key={index}>
+                    <Card
+                      sx={{
+                        boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
+                        borderRadius: "8px",
+                      }}
+                    >
+                      <CardContent>
+                        <Typography variant="body2" sx={{ mb: 1 }}>
+                          {firstSegment.departure.iataCode} <FaPlaneDeparture />
+                          {"  ---------------------------   "}
+                          <FaPlaneArrival />
+                          {lastSegment.arrival.iataCode}
+                          <br />
+                          <strong>Departure:</strong> {departureTime} |{" "}
+                          <strong>Arrival:</strong> {arrivalTime}
+                          <br />
+                          Live Price:ZAR {totalPriceInZar}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                );
+              })}
+            </Grid>
+          )}
         </CardContent>
       </Card>
     </Box>
